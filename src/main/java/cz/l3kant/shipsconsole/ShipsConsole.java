584d104b619f66c8a442f99ea672575dab4537db
L3kant <st.voboril@gmail.com>
2022-10-23 11:36:36 +0200

    init
    init commit

---
+++ src/main/java/cz/l3kant/shipsconsole/ShipsConsole.java
@@ -0,0 +1,16 @@
+package cz.l3kant.shipsconsole;
+
+/**
+ *
+ * @author l3kan
+ */
+public class ShipsConsole {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
